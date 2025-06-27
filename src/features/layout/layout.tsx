afe5f9afd8b096010aa09ab3de47f5ce8ce0fd91
madiaslanov <asmadi.rus@mail.ru>
2025-06-27 12:14:30 +0500

    nav up

--- src/features/layout/layout.tsx
+++ src/features/layout/layout.tsx
@@ -1,22 +1,8 @@
 import {Link, Outlet, useLocation} from "react-router-dom";
 import style from "./layout.module.css"
-import {useEffect} from "react";
 
 export default function Layout() {
     const location = useLocation()
-
-    useEffect(() => {
-        const handleFocus = () => document.body.classList.add("keyboard-open");
-        const handleBlur = () => document.body.classList.remove("keyboard-open");
-
-        window.addEventListener("focusin", handleFocus);
-        window.addEventListener("focusout", handleBlur);
-
-        return () => {
-            window.removeEventListener("focusin", handleFocus);
-            window.removeEventListener("focusout", handleBlur);
-        };
-    }, []);
 
     return (
         <div className={style.appContainer}>
