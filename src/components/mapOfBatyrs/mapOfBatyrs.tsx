2aa47c5a3703c1fba2af31a231b805241dc04b6f
madiaslanov <asmadi.rus@mail.ru>
2025-07-07 02:05:12 +0500

    add: batyr map page

--- src/components/mapOfBatyrs/mapOfBatyrs.tsx
+++ src/components/mapOfBatyrs/mapOfBatyrs.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
-import style from './MapOfBatyrs.module.css';
+import style from './mapOfBatyrs.module.css';
 
 // Объявляем нашу будущую глобальную функцию, чтобы TypeScript ее "видел"
 declare global {
